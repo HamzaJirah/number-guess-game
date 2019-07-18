a6b6a942c6d3650ee06091d16758695d3f20a1ef
HamzaJirah <hamzajirah@gmail.com>
2019-07-18 13:40:20 +0100

    declared global variables

---
+++ app.js
@@ -0,0 +1,12 @@
+// global variables
+const min = 1;
+const max = 20;
+const winningNum = 2;
+
+// Get reference to DOM elements
+const minimumNum = document.querySelector('.min-num');
+const maximumNum = document.querySelector('.max-num');
+
+// set text content of minimumNum and maximumNum
+minimumNum.textContent = min;
+maximumNum.textContent = max;
