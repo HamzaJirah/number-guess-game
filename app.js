c890427900472d595b073d0b70bd1baf52c9f094
HamzaJirah <hamzajirah@gmail.com>
2019-07-19 12:39:22 +0100

    added event listener to submit button

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ const minimumNum = document.querySelector('.min-num');
 const maximumNum = document.querySelector('.max-num');
 const guess = document.querySelector('#guess-number');
 const submitGuess = document.querySelector('#submit-guess');
+const message = document.querySelector('#score-update');
 
 // set text content of minimumNum and maximumNum
 minimumNum.textContent = min;
 maximumNum.textContent = max;
+
+// listen for user submission
+submitGuess.addEventListener('click', () => {
+  
+})
