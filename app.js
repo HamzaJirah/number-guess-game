6cc25d4491ae04c368e582e2b0946e18678b4cfb
HamzaJirah <hamzajirah@gmail.com>
2019-07-19 16:26:00 +0100

    logged random number to console

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ submitGuess.addEventListener('mousedown', () => {
 // generate random number 
 function getRandomNum(){
   Math.floor(Math.random() * (max-min) +1);
+  console.log(Math.floor(Math.random() * (max-min) +1));
 }
 
 // Play game 
